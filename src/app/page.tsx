036b1af56a3fb561c83cf67210b62040d1d780d9
Digamber Bhati <93906020+Digamberbhati@users.noreply.github.com>
2024-07-25 10:50:37 +0530

    demo
    website

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Home } from '@/pages/Home'
+import React from 'react'
+
+const homePage = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+
+export default homePage
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
+
+  return (
+
+    <div className="flex items-center justify-between backdrop-blur-lg  fixed  top-15 w-full px-8 py-4 ">
+      <img className="h-24" src="https://greatertechhub.com/assets/img/logo/logo.png" alt="" />
+      <div
+      className={cn(" ", className)}
+    >
+      <Menu setActive={setActive} >
+        <Link href={"/"} >Home</Link>
+        <Link href={"/about"} >About</Link>
+        <Link href={"/service"} >Service</Link>
+        <Link href={"/contact"} >Contact</Link>
+        <Link href={"/company"} >Company</Link>
+        <MenuItem setActive={setActive} active={active} item="Pricing">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/event">Event</HoveredLink>
+            <HoveredLink href="/team">Team</HoveredLink>
+            <HoveredLink href="/blog">Blog</HoveredLink>
+            <HoveredLink href="/client">Client</HoveredLink>
+            <HoveredLink href="/careers">Careers</HoveredLink>
+            <HoveredLink href="/support">Support</HoveredLink>
+          </div>
+        </MenuItem>
+      </Menu>
+    </div>
+      <Link href={"#"} className="border-2 h-6 flex justify-center items-center border-black bg-black p-4 rounded-full ">Join us</Link>
+    </div>
+  )
+}
+
+export default Navbar
