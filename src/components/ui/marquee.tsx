af26fd52ef89f80c348e0a230212e8939f61a1ed
akash <akashmanjhi1111198@gmail.com>
2024-07-27 23:58:50 +0530

    complete marquee

--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -29,16 +29,14 @@ const MarqueeComponent = () => {
         <div className='w-full py-20 mt-32 overflow-hidden bg-white flex flex-col gap-8'>
             <Marquee pauseOnHover speed={70} >
             {images.map((item, index) => (
-                    <Image
-                    loading='lazy'
-                    key={index}
-                    src={item}
-                    style={{ aspectRatio: '3 / 2', objectFit: 'contain' }}
-                    className='grayscale'
-                    width={200}
-                    height={100}
-                    alt="All About"
-                />
+                <Image
+                key={index}
+                src={item}
+                width={200}
+                height={200}
+                className='grayscale object-contain w-24 h-12 sm:w-32 sm:h-16 md:w-48 md:h-24 lg:w-64 lg:h-32'
+                alt={`Image ${index + 1}`}
+            />
                 
                 ))}
             </Marquee>
@@ -48,11 +46,10 @@ const MarqueeComponent = () => {
                     <Image
                     key={index}
                     src={item}
-                    style={{ aspectRatio: '3 / 2', objectFit: 'contain' }}
-                    className='grayscale'
                     width={200}
-                    height={100}
-                    alt="All About"
+                    height={200}
+                    className='grayscale object-contain w-24 h-12 sm:w-32 sm:h-16 md:w-48 md:h-24 lg:w-64 lg:h-32'
+                    alt={`Image ${index + 1}`}
                 />
                 
                 ))}
