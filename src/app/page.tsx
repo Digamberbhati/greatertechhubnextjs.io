0a827907fdd3a171e398b82752478f1a1e0a6e74
Bablumahto <bablumht18@gmail.com>
2024-08-13 16:00:28 +0530

    home_page_changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 
-import { Home } from '@/pages/Home'
+
+import  Home  from '@/pages/Home'
 import React from 'react'
 
 const homePage = () => {
   return (
-    <div>
+    <>
       <Home/>
       
-    </div>
+    </>
   )
 }
 
