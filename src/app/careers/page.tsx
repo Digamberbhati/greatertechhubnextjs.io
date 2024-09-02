6ea0dc2bf4dde15649a69ceac424fac7026c50ac
Digamber Bhati <93906020+Digamberbhati@users.noreply.github.com>
2024-09-02 16:32:51 +0530

    ok

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -6,9 +6,314 @@ const careerPage = () => {
     <div className='py-32'>
       <h1 className='text-3xl text-[#F5EBEB]  w-full text-center py-8 font-semibold'> Careers</h1>
 
+      <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Internship</h1>
+            <p className="leading-relaxed mb-3">.</p>
+            <div className="flex items-center flex-wrap ">
+              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              {/* <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                  <circle cx="12" cy="12" r="3"></circle>
+                </svg>1.2K
+              </span>
+              <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>6
+              </span> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog"/>
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Entry-Leval</h1>
+            <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <div className="flex items-center flex-wrap">
+              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                  <circle cx="12" cy="12" r="3"></circle>
+                </svg>1.2K
+              </span>
+              <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>6
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"/>
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+            <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <div className="flex items-center flex-wrap ">
+              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                  <circle cx="12" cy="12" r="3"></circle>
+                </svg>1.2K
+              </span>
+              <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>6
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <div className='max-w-screen-lg mx-auto'>
   
-<p >info@greatertechhub.com</p>
+
+<div className="w-full md:w-96 md:max-w-full mx-auto">
+  <div className="p-6 border border-gray-600 sm:rounded-md bg-gray-800">
+    <form
+     
+    >
+      <label className="block mb-6">
+        <span className="text-gray-300">Your name</span>
+        <input
+          required
+          name="name"
+          type="text"
+          className="
+            block
+            w-full
+            mt-1
+            border-gray-600
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+            bg-transparent
+            placeholder-gray-600
+            text-gray-300
+          "
+          placeholder="Joe Bloggs"
+        />
+      </label>
+      <label className="block mb-6">
+        <span className="text-gray-300">Email address</span>
+        <input
+          required
+          name="email"
+          type="email"
+          className="
+            block
+            w-full
+            mt-1
+            border-gray-600
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+            bg-transparent
+            placeholder-gray-600
+            text-gray-300
+          "
+          placeholder="joe.bloggs@example.com"
+        />
+      </label>
+      <label className="block mb-6">
+        <span className="text-gray-300">Years of experience</span>
+        <select
+          required
+          name="experience"
+          className="
+            block
+            w-full
+            mt-1
+            border-gray-600
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+            bg-transparent
+            placeholder-gray-600
+            text-gray-300
+          "
+        >
+          <option>Less than a year</option>
+          <option>1 - 2 years</option>
+          <option>2 - 4 years</option>
+          <option>4 - 7 years</option>
+          <option>7 - 10 years</option>
+          <option>10+ years</option>
+        </select>
+      </label>
+      <label className="block mb-6">
+        <span className="text-gray-300">Tell us more about yourself</span>
+        <textarea
+          name="message"
+          className="
+            block
+            w-full
+            mt-1
+            border-gray-600
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+            bg-transparent
+            placeholder-gray-600
+            text-gray-300
+          "
+          rows="3"
+          placeholder="What motivates you?"
+        ></textarea>
+      </label>
+      <label className="block mb-6">
+        <span className="text-gray-300">Your CV</span>
+        <input
+          required
+          name="cv"
+          type="file"
+          className="
+            block
+            w-full
+            mt-1
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+            bg-transparent
+            placeholder-gray-600
+            text-gray-300
+          "
+        />
+      </label>
+      <div className="mb-6">
+        <div className="mt-2">
+          <div>
+            <label className="inline-flex items-center">
+              <input
+                name="remote"
+                value="yes"
+                type="radio"
+                className="
+                  text-indigo-600
+                  border-gray-600
+                  rounded-full
+                  shadow-sm
+                  focus:border-indigo-300
+                  focus:ring
+                  focus:ring-offset-0
+                  focus:ring-indigo-200
+                  focus:ring-opacity-50
+                  bg-transparent
+                  placeholder-gray-600
+                  text-gray-300
+                "
+                checked
+              />
+              <span className="ml-2 text-gray-300"
+                >You'd like to work remotely</span
+              >
+            </label>
+          </div>
+          <div>
+            <label className="inline-flex items-center">
+              <input
+                name="remote"
+                value="no"
+                type="radio"
+                className="
+                  text-indigo-600
+                  border-gray-600
+                  rounded-full
+                  shadow-sm
+                  focus:border-indigo-300
+                  focus:ring
+                  focus:ring-offset-0
+                  focus:ring-indigo-200
+                  focus:ring-opacity-50
+                  bg-transparent
+                  placeholder-gray-600
+                  text-gray-300
+                "
+              />
+              <span className="ml-2 text-gray-300"
+                >You'd prefer to work onsite</span
+              >
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="mb-6">
+        <button
+          type="submit"
+          className="
+            h-10
+            px-5
+            text-indigo-100
+            bg-indigo-700
+            rounded-lg
+            transition-colors
+            duration-150
+            focus:shadow-outline
+            hover:bg-indigo-800
+          "
+        >
+          Apply
+        </button>
+      </div>
+      <div>
+        <div className="mt-2 text-gray-300 text-right text-xs">
+          by
+          <a href="https://herotofu.com" className="hover:underline" target="_blank"
+            >HeroTofu</a
+          >
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
 </div>
     </div>
   )
