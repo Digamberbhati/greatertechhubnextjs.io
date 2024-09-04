7fec3d173723c4b848c3f501fe3898a1e9827cdb
Bablumahto <bablumht18@gmail.com>
2024-09-04 15:26:44 +0530

    ok

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactPage = () => {
   return (
-    <div className=' overflow-x-hidden mt-56'>
+    <div className=' overflow-x-hidden'>
 
       <div className="max-w-5xl max-lg:max-w-3xl mx-auto bg-white my-6 font-[sans-serif] py-8">
         <div className="text-center px-6">
@@ -162,38 +162,7 @@ const ContactPage = () => {
                     </g>
                   </svg>
                 </div>
-{/* 
-                <div className="col-span-full">
-                  <h6 className="text-sm text-gray-800">Select Subject</h6>
-               <div className="flex max-lg:flex-col gap-6 mt-4"> */}
-                    {/* <div className="flex items-center">
-                      <input id="radio1" type="radio" name="value1" className="hidden peer" checked />
-                      <label for="radio1"
-                        className="relative p-0.5 flex items-center justify-center shrink-0 peer-checked:before:hidden before:block before:absolute before:w-full before:h-full before:bg-white w-5 h-5 cursor-pointer border-2 border-[#011c2b] rounded-full overflow-hidden">
-                        <span className="border-[4px] border-[#011c2b] rounded-full w-full h-full"></span>
-                      </label>
-                      <p className="text-sm text-gray-500 ml-4">General Inquiry</p>
-                    </div> * */}
 
-                     {/* <div className="flex items-center">
-                      <input id="radio2" type="radio" name="value1" className="hidden peer" />
-                      <label for="radio2"
-                        class="relative p-0.5 flex items-center justify-center shrink-0 peer-checked:before:hidden before:block before:absolute before:w-full before:h-full before:bg-white w-5 h-5 cursor-pointer border-2 border-[#011c2b] rounded-full overflow-hidden">
-                        <span className="border-[4px] border-[#011c2b] rounded-full w-full h-full"></span>
-                      </label>
-                      <p className="text-sm text-gray-500 ml-4">Technical Support</p>
-                    </div>  */}
-
-                     {/* <div className="flex items-center">
-                      <input id="radio3" type="radio" name="value1" className="hidden peer" />
-                      <label for="radio3"
-                        class="relative p-0.5 flex items-center justify-center shrink-0 peer-checked:before:hidden before:block before:absolute before:w-full before:h-full before:bg-white w-5 h-5 cursor-pointer border-2 border-[#011c2b] rounded-full overflow-hidden">
-                        <span className="border-[4px] border-[#011c2b] rounded-full w-full h-full"></span>
-                      </label>
-                      <p className="text-sm text-gray-500 ml-4">Website Feedback</p>
-                    </div> * */}
-                   {/* </div>
-                </div> */}
               </div> 
 
               <button type="button"
