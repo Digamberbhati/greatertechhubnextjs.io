a0577c69a7ad28f3d04d9dd8caac0bcf30d3220a
Bablumahto <bablumht18@gmail.com>
2024-09-04 11:24:45 +0530

    ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,19 +22,22 @@ const images = [
     {
       text: "Tech",
     },
+
     {
       text: "Solutions",
     },
+
     {
       text: "for ",
     },
+    
     {
       text: "Tomorrow’s",
     },
    
     {
       text: "Success.",
-      className: "text-blue-500 dark:text-blue-500",
+      className: "text-blue-500 dark:text-blue-600",
     },
   ];
 
@@ -96,7 +99,7 @@ const images = [
           <p className="mb-6">Get exclusive access to our latest updates and offers.</p>
           <a
             href="#"
-            className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded shadow-md transition duration-300"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded shadow-md transition duration-300"
           >
             Contact Us
           </a>
