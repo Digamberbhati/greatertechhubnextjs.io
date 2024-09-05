0c94eddf35b97dce9035ab344d0fde72fe885e09
akash <akashmanjhi1111198@gmail.com>
2024-09-05 14:09:15 +0530

    fix pngs and add favicon

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -8,7 +8,7 @@ const ClientPage = () => {
   return (
     
 
-    <div className='bg-white'>
+    <div className='bg-white mt-8'>
       <h1 className='text-3xl text-black  w-full text-center pt-20 font-semibold'>Our Clients</h1>
 <Marquee/>
     </div>
--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -105,7 +105,7 @@ const supportPage = () => {
        
       </div>
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 z-50 w-5/6">
       <img className="object-cover object-center rounded" alt="hero" src="/images/reffral.png"/>
     </div>
   </div>
