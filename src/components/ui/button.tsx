56a1545ee1cea368a73c3567b538a5bc6c723cab
Bablumahto <bablumht18@gmail.com>
2024-09-06 12:32:55 +0530

    ok

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ function Button(props:any) {
   return (
     <>
       <button className="bg-gray-800 duration-300 text-white font-bold py-2 px-4 rounded hover:bg-transparent hover:bg-transparent ">
-  <Link href='contact'>{props.text}</Link>
+  <Link href='/contact'>{props.text}</Link>
 </button>
         </>
     
